6347ac13bf0e8fed8f80b27355e1a996570dd614
Owen-Morgan825 <owen.morgan092@gmail.com>
2026-04-02 18:44:28 -0700

    keep pinging NT until it works

--- ntinit.py
+++ ntinit.py
@@ -3,41 +3,48 @@ import ntcore
 
 def getNT(clientName: str):
     ntconf = open("ntconfig.txt")
+    retryTime = float(ntconf.readline())
+    print("Wait time: " + str(retryTime) + " seconds")
     robotIP = str(ntconf.readline())
     print("robot address: " + robotIP)
     localIP = str(ntconf.readline())
     print("local address: " + localIP)
     ntconf.close()
+
+
     time.sleep(1)
-    print("Scanning for NetworkTables...")
-    time.sleep(1)
-    inst = ntcore.NetworkTableInstance.getDefault()
-    inst.startClient4(clientName)
-    inst.setServer(robotIP)
-    robotConnected = inst.getTable("fuelDetector").getBooleanTopic("robotConnected").subscribe(False)
-    time.sleep(0.1)
-    connected = robotConnected.get()
-    time.sleep(0.1)
-    time.sleep(0.1) # Don't ask. I don't know.
-    connected = robotConnected.get()
-    print("robot connection found? " + str(connected))
-    if(not(connected)):
-        inst.setServer(localIP)
-        time.sleep(0.1)
-        time.sleep(0.1)
+    i = 0
+    startTime = time.time()
+    while((time.time() - startTime) < retryTime):
+        print("Scanning for NetworkTables... try number " + str(i + 1))
+        time.sleep(1)
+        inst = ntcore.NetworkTableInstance.getDefault()
+        inst.startClient4(clientName)
+        inst.setServer(robotIP)
+        robotConnected = inst.getTable("fuelDetector").getBooleanTopic("robotConnected").subscribe(False)
         time.sleep(0.1)
         connected = robotConnected.get()
-        print()
-        print("local connection found? " + str(connected))
-        if(connected):
-            print("Server found at " + localIP + " (local)")
-            return inst
+        time.sleep(0.1)
+        time.sleep(0.1) # Don't ask. I don't know.
+        connected = robotConnected.get()
+        print("robot connection found? " + str(connected))
+        if(not(connected)):
+            inst.setServer(localIP)
+            time.sleep(0.1)
+            time.sleep(0.1)
+            time.sleep(0.1)
+            connected = robotConnected.get()
+            print()
+            print("local connection found? " + str(connected))
+            if(connected):
+                print("Server found at " + localIP + " (local)")
+                return inst
         else:
-            print("No NetworkTables server detected. Check IP addresses.")
+            print("Server found at " + robotIP + " (robot)")
             return inst
-    else:
-        print("Server found at " + robotIP + " (robot)")
-        return inst
+        i += 1
+    print("No NetworkTables server detected. Check IP addresses.")
+    return inst
 
 def testNT(): 
     #Test code
