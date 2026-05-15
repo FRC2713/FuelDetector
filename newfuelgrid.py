f5ec24caa8592e434ead4027fe0f23a4d03d8d0f
Owen-Morgan825 <owen.morgan092@gmail.com>
2026-05-15 09:42:50 -0400

    Update newfuelgrid.py

--- newfuelgrid.py
+++ newfuelgrid.py
@@ -1,3 +1,7 @@
+import time
+import ntinit
+import MultilayerGrid
+
 nst = ntinit.getNT("clusterClient")
 time.sleep(1) # Make a delay long enough to read
 
@@ -6,6 +10,12 @@ fuelValues = fuelTable.getStringTopic("fuelData").subscribe("")
 fuelHeading= fuelTable.getDoubleTopic("clusterHeading").publish()
 totalFuel = fuelTable.getIntegerTopic("totalFuel").publish()
 
-class FuelClustering {}
+class FuelClustering:
+  fuel_chance_threshold: float = 0.70 #Minimum confidence for fuel to be considered
+  def __init__(self, imgWidth: int, imgHeight: int, FOV: float, grid: MultilayerGrid):
+    self.imageWidth: int = imgWidth;
+    self.imageHeight: int = imgHeight;
+    self.FOV: float = FOV;
+    
 
 print("Program started")
